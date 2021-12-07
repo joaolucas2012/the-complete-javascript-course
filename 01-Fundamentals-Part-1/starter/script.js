2744368e7981a4403291080fd510b45e5fdb0e9c
unknown <ribeirojoaolucas68@gmail.com>
2021-12-07 16:16:43 -0300

    feat: learning types of variables and initial concepts

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,63 @@
+/* let js = 'amazing';
+if (js === 'amazing') alert('Javascript is fun');
+
+console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log(23); */
+
+// Variable name conventions
+let firstName = "Jonas";
+console.log(firstName);
+
+/* 1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country(population in millions) */
+let country = 'Brazil';
+let continent = "South America";
+let population = 7000000;
+
+/* 2. Log their values to the console */
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// Type of values
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof country);
+console.log(typeof 'Jonas');
+
+/* BOOLEAN */
+true;
+console.log(true);
+let javascripIsFun = true;
+console.log(javascripIsFun);
+
+/* Reassign the variable type */
+// DINAMIC TYPING //
+javascriptIsFun = 'YES!';
+console.log(javascriptIsFun);
+
+// UNDEFINED TYPE //
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1991;
+console.log(typeof year);
+
+// JAVASCRIPT ERROR //
+/* Null type isn't an object */
+console.log(typeof null);
+
+
+/* 1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet */
+let isIsland = false;
+let language;
+
+/*2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console */
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
