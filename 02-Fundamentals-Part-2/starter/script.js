ebf5c74306b2732b3d3626bfb810423b58fb3df5
unknown <ribeirojoaolucas68@gmail.com>
2021-12-20 20:45:56 -0300

    feat: learning arrow functions and functions calling other functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -87,9 +87,9 @@ represents. For example, China has 1441 million people, so it's about 18.2% of t
 4. Create a function expression which does the exact same thing, called 'percentageOfWorld2', and also call it with 3 country populations (can be the same populations) */
 
 //1 and 2
-// function percentageOfWorld1(population) {
-//   return ((population / 7900) * 100);
-// }
+function percentageOfWorld1(population) {
+  return ((population / 7900) * 100);
+}
 
 //3
 // const BrazilPercentage = percentageOfWorld1(212);
@@ -106,5 +106,64 @@ represents. For example, China has 1441 million people, so it's about 18.2% of t
 // const CanadaPercentage = percentageOfWorld2(38);
 // const CaribbeanPercentage = percentageOfWorld2(44.4);
 // console.log(`Ireland: ${IrelandPercentage.toPrecision(2)}%, Canada: ${CanadaPercentage.toPrecision(2)}%, Caribbean: ${CaribbeanPercentage.toPrecision(2)}%.`);
+
+///////////////////////////////////////////////////
+
+// ARROW FUNCTION
+// const calcAge = birthYear => 2037 - birthYear;
+// const age = calcAge(2000);
+// console.log(age);
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//   const age = 2021 - birthYear;
+//   const retirement = 65 - age;
+//   // return retirement;
+//   return `${firstName} retires in ${retirement} years.`;
+// }
+
+// console.log(yearsUntilRetirement(2000, 'João Lucas'));
+// console.log(yearsUntilRetirement(1996, 'Carlos'));
+
+/* 1. Recreate the last assignment, but this time create an arrow function called
+'percentageOfWorld3' */
+
+// const percentageOfWorld3 = population => ((population / 7900) * 100);
+// const IrelandPercentage = percentageOfWorld3(4.9);
+// console.log(`Ireland: ${IrelandPercentage.toFixed(2)}%.`);
+
+///////////////////////////////////////////////////
+
+// FUNCTIONS CALLING OTHER FUNCTIONS
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
+
+//   const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+//   return juice;
+// }
+
+// console.log(fruitProcessor(2, 3));
+
+
+/* 1. Create a function called 'describePopulation'. Use the function type you
+like the most. This function takes in two arguments: 'country' and
+'population', and returns a string like this: 'China has 1441 million people,
+which is about 18.2% of the world.'
+2. To calculate the percentage, 'describePopulation' call the
+'percentageOfWorld1' you created earlier
+3. Call 'describePopulation' with data for 3 countries of your choice */
+
+//1
+// const describePopulation = (country, population) => {
+//   return `${country} has ${population} million people, which is about ${percentageOfWorld1(population).toFixed(2)}% of the world.`;
+// }
+
+// console.log(describePopulation('Brazil', 212));
+// console.log(describePopulation('Portugal', 10));
+// console.log(describePopulation('Ireland', 4.9));
 
 ///////////////////////////////////////////////////
