3702b6ed7bea857ea8d6d436671881d79f890617
unknown <ribeirojoaolucas68@gmail.com>
2021-12-20 15:13:16 -0300

    feat: learning use strict

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive');
+
+// I CAN'T NAME VARIABLES LIKE THIS
+// const interface = 'Audio';
+// const private = 534;
+// const if = 23;
