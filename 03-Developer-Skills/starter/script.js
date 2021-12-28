c2bae84758421caab4592101c31366a084ee22ee
unknown <ribeirojoaolucas68@gmail.com>
2021-12-28 15:49:51 -0300

    feat: instructions to install live-server and Node.JS

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -26,3 +26,34 @@ const calcAge = birthYear => 2037 - 2001;
 // console.log();
 
 ///////////////////////////////////////////////////
+
+// INSTALLING NODE.JS AND SETTING UP A DEV ENVIRONMENT
+
+// Those tools reload the html pages automatically
+
+// Install Live Server Extension in VS Code
+// Install Node.JS
+
+/*
+// DISCOVER THE VERSION OF NODE.JS I HAVE INSTALLED:
+--> Just open the terminal and type:
+node -v
+
+// INSTALL LIVE SERVER ON THE TERMINAL:
+--> Just go to the terminal and type:
+npm install live-server -g
+--> If it's on MAC, you have to type:
+sudo npm install live-server -g
+
+// TO USE THE LIVE SERVER VIA THE TERMINAL:
+--> Just go to the terminal and type:
+live-server
+
+// EXPLANATION ABOUT THE INSTALLATION:
+--> npm is the Node Package Manager, a program for downloading tools.
+--> Then comes install, which is the command to install something.
+--> Then comes the name of the tool we want to install.
+--> -g means the tool must be installed globally.
+*/
+
+///////////////////////////////////////////////////
