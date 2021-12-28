2fd1c0a08013ca560bc90084c832cefadf860f74
unknown <ribeirojoaolucas68@gmail.com>
2021-12-28 15:20:15 -0300

    first commit

---
+++ 03-Developer-Skills/starter/script.js
@@ -0,0 +1,28 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+///////////////////////////////////////////////////
+
+// SETTING UP PRETTIER AND VS CODE
+// The Prettier Extension transform every single quotes in double quotes by default
+
+/*
+const x = '23';
+if (x === 23) console.log(23);
+
+const calcAge = birthYear => 2037 - 2001;
+*/
+
+// Go to user snippets in the VS Code, and set:
+/*
+"Print to console": {
+  "scope": "javascript,typescript",
+  "prefix": "log",
+  "body": ["console.log();"],
+  "description": "Log output to console"
+}
+*/
+// Every time I write "log", I can press enter and it will be replaced by "console.log();"
+// console.log();
+
+///////////////////////////////////////////////////
