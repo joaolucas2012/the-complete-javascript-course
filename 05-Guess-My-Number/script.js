ba4fd33bf9cfc65aa79fa3900559f3df76bb5a5f
unknown <ribeirojoaolucas68@gmail.com>
2022-01-06 00:27:22 -0300

    feat: implement the game logic

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -29,6 +29,10 @@ console.log(document.querySelector('.guess').value);
 
 // HANDLING CLICK EVENTS
 
+// Defining the number between 1 and 20 randomly
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
 // Selecting the button element
 document.querySelector('.check').addEventListener('click', function () {
   // The returned value of an input is always a string, so we need in this case to convert it to a number.
@@ -37,5 +41,25 @@ document.querySelector('.check').addEventListener('click', function () {
   // Ever check first if there is a value in the input
   if (!guess) {
     document.querySelector('.message').textContent = '🚫No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '🧨Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🥺You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '🤏Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🥺You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
