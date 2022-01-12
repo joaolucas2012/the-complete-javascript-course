1123f291224a98928d9fb9ba620ff2f39b6a7a7d
unknown <ribeirojoaolucas68@gmail.com>
2022-01-11 22:11:36 -0300

    first commit

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,1 +1,24 @@
 'use strict';
+/***************** GLOBAL SCOPE *****************/
+
+// Function defined in the global scope
+function calcAge(birthYear) {
+  // FUNCTION SCOPE
+  const age = 2037 - birthYear;
+
+  // This function can access the global variables
+  // console.log(firstName);
+
+  function printAge() {
+    // ANOTHER FUNCTION SCOPE
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+// Global variables
+const firstName = 'Jonas';
+calcAge(1991);
