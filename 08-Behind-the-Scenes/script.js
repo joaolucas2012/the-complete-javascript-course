a170c1047c6fe297b5e490f98c98c1c83dc3598d
unknown <ribeirojoaolucas68@gmail.com>
2022-01-11 23:23:59 -0300

    feat: learning how the scope works behind the scenes

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -11,7 +11,40 @@ function calcAge(birthYear) {
 
   function printAge() {
     // ANOTHER FUNCTION SCOPE
-    const output = `You are ${age}, born in ${birthYear}`;
+    // This function can access the parent function variables and global variables
+    let output = `${firstName}, You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      // BLOCK SCOPE
+      var millenial = true;
+
+      /* Creating NEW variable with same name as outer scope's variable */
+
+      // The Javascript will search for the firstName in the current scope, and it is in the current scope, so the firstName will be 'Steven' instead 'Jonas'
+      const firstName = 'Steven';
+
+      // Reassigning outer scope variables
+      output = 'NEW OUTPUT!';
+
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      // This add function can just be accessed inside of this block scope
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // The function scope cannot access the block scope variables
+    // console.log(str);
+
+    // var variables can be accessed outside the block scope, cause they are function scoped
+    console.log(millenial);
+
+    // The function scope cannot access the block scope functions (just in strict mode)
+    // add(2, 3);
+
+    // The output was changed inside the block scope
     console.log(output);
   }
   printAge();
@@ -22,3 +55,7 @@ function calcAge(birthYear) {
 // Global variables
 const firstName = 'Jonas';
 calcAge(1991);
+
+// Global scope cannot access variables declared in the function scopes (inner functions)
+// console.log(age);
+// printAge();
