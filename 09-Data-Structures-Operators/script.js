134611b6a5f224696015bde27f2f062ca3711d96
unknown <ribeirojoaolucas68@gmail.com>
2022-01-18 18:56:53 -0300

    feat: learn how to destructure objects

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -6,7 +6,7 @@ const flights =
 
 // Data needed for first part of the section
 const restaurant = {
-  name: 'Classico Italiano',
+  name: 'Clássico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
@@ -14,6 +14,17 @@ const restaurant = {
 
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.`
+    );
   },
 
   openingHours: {
@@ -32,6 +43,10 @@ const restaurant = {
   },
 };
 
+///////////////////////////////////////////////////
+
+// DESTRUCTURING ARRAYS
+/*
 // Here, the x, y and z variables received the values 2, 3 and 4 from the 'arr' array
 const arr = [2, 3, 4];
 // const a = arr[0];
@@ -77,3 +92,92 @@ console.log(i, j, k);
 // Here, the variables p and q received the values ​​8 and 9, and as there was no third value to be received, the variable r persisted with the value 1, already defined previously by default.
 const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r);
+*/
+
+///////////////////////////////////////////////////
+
+// DESTRUCTURING OBJECTS
+
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+// Doing the same but with the properties renamed
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+// It's necessary to put it inside of parêntesis
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested objects
+const {
+  fri: { open: o, close: c },
+} = hours;
+console.log(o, c);
+
+/*
+// Exercising
+const objectName = {
+  name: 'João Lucas',
+  friends: {
+    fst: 'Jane',
+    snd: 'Leticia',
+    trd: 'Marcos',
+  },
+};
+
+const {
+  name: n,
+  friends: { fst: one, trd: two },
+} = objectName;
+console.log(`${n}, ${one}, ${two}`);
+*/
+
+// Calling functions passing an destructured object as a parameter
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'Via del Sole, 21',
+  starterIndex: 1,
+});
+
+/*
+// Exercising
+const heroStory = {
+  heroes: ['Ben 10', 'Omniman', 'Generator Rex'],
+  story: function ({ hero1, hero2, place = 'some place in this city' }) {
+    console.log(
+      `Oh my God! ${this.heroes[hero1]} and ${this.heroes[hero2]} are fighting at ${place}.`
+    );
+  },
+};
+
+heroStory.story({
+  hero1: 0,
+  hero2: 2,
+  place: 'New York',
+});
+
+heroStory.story({
+  hero1: 2,
+  hero2: 1,
+});
+*/
+///////////////////////////////////////////////////
